c4ce4d322f4f41c9bbd32df592843a13c4a73576
Navid Dezashibi <dezashibi@gmail.com>
2019-12-27 01:01:46 +0330

    Added graphql api and graphql client

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyD1BpiemCfF3UAQq-uKbwgweLpOpxMk4HM",
+  authDomain: "crwn-db-27f49.firebaseapp.com",
+  databaseURL: "https://crwn-db-27f49.firebaseio.com",
+  projectId: "crwn-db-27f49",
+  storageBucket: "crwn-db-27f49.appspot.com",
+  messagingSenderId: "880380744687",
+  appId: "1:880380744687:web:9ef31bbd8794c8a7225c1a",
+  measurementId: "G-4F84D2HE5S"
 };
 
 firebase.initializeApp(config);
